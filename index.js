254774ed6b89b027d54ac16e1ee48a9ff8ccd634
James Booth <jabooth@gmail.com>
2016-12-11 15:10:53 +0000

    move to ES2015 style class

--- index.js
+++ index.js
@@ -1,161 +1,155 @@
+'use strict'
+
 const icy = require('icy')
 const url = require('url')
 const http = require('http')
 const lame = require('lame')
-const util = require('util')
 
 const CHANNELS = 2
 const SAMPLE_SIZE = 16
 const SAMPLE_RATE = 44100
 const META_INTERVAL = 8192
 
-function removeValueFromArray (array, value) {
-  const index = array.indexOf(value)
-  if (index >= 0) {
-    array.splice(index, 1)
-  }
+function removeValueFromArray(array, value) {
+    const index = array.indexOf(value)
+    if (index >= 0) {
+        array.splice(index, 1)
+    }
 }
 
-function Nicercast (inputStream, opts) {
-  opts = opts || {}
+class Nicercast {
 
-  this._server = http.createServer(this._handleRequest.bind(this))
-  this._inputStream = inputStream
-  this._metadata = opts.metadata || 'Nicercast'
+    constructor(inputStream, opts) {
 
-  this._listenStreams = []
-  this._metadataStreams = []
+        opts = opts || {}
+
+        this._server = http.createServer(this._handleRequest.bind(this))
+        this._inputStream = inputStream
+        this._metadata = opts.metadata || 'Nicercast'
+
+        this._listenStreams = []
+        this._metadataStreams = []
+    }
+
+    _handleRequest(req, res) {
+        switch (req.url) {
+            case '/':
+            case '/listen.m3u':
+                if (req.method === 'GET') {
+                    this._playlist(req, res)
+                } else {
+                    res.writeHead(405)
+                    res.end('Method not allowed')
+                }
+
+                break
+            case '/listen':
+                if (req.method === 'GET') {
+                    this._listen(req, res)
+                } else {
+                    res.writeHead(405)
+                    res.end('Method not allowed')
+                }
+
+                break
+            default:
+                res.writeHead(404)
+                res.end('Not found')
+        }
+    }
+
+    _playlist(req, res) {
+        const urlProps = {
+            protocol: 'http',
+            pathname: '/listen'
+        }
+
+        if (req.headers.host) {
+            urlProps.host = req.headers.host
+        } else {
+            const info = req.socket.address()
+            urlProps.hostname = info.address
+            urlProps.port = info.port
+        }
+
+        res.writeHead(200, { 'Content-Type': 'audio/x-mpegurl' })
+        res.end(url.format(urlProps))
+    }
+
+    _listen(req, res) {
+        const acceptsMetadata = (req.headers['icy-metadata'] === '1')
+
+        res.writeHead(200, {
+            'Connection': 'close',
+            'Content-Type': 'audio/mpeg',
+            'Icy-Metaint': (acceptsMetadata ? META_INTERVAL : undefined)
+        })
+
+        let output
+        if (acceptsMetadata) {
+            output = new icy.Writer(META_INTERVAL)
+            this._metadataStreams.push(output)
+            output.queueMetadata(this._metadata)
+            output.pipe(res)
+        } else {
+            output = res
+        }
+
+        const encoder = new lame.Encoder({
+            channels: CHANNELS,
+            bitDepth: SAMPLE_SIZE,
+            sampleRate: SAMPLE_RATE
+        })
+
+        this._listenStreams.push(encoder)
+
+        function teardown() {
+            removeValueFromArray(this._listenStreams, encoder)
+            removeValueFromArray(this._metadataStreams, output)
+
+            this._inputStream.unpipe(encoder)
+            encoder.unpipe(output)
+            encoder.end()
+        }
+
+        this._inputStream.pipe(encoder).pipe(output)
+        req.socket.on('close', teardown.bind(this))
+    }
+
+    setMetadata(metadata) {
+        this._metadata = metadata
+
+        this._metadataStreams.forEach(function (stream) {
+            stream.queue(metadata)
+        })
+    }
+
+    setInputStream(inputStream) {
+        const currentInput = this._inputStream
+
+        this._listenStreams.forEach(function (stream) {
+            currentInput.unpipe(stream)
+            inputStream.pipe(stream)
+        })
+
+        this._inputStream = inputStream
+    }
+
+    listen(port, callback) {
+        this._server.listen(port, () => {
+            // If a port of 0 is provided, the server will pick an appropriate port.
+            // ensure we invoke our callback with whatever port is running
+            callback(this._server.address().port)
+        })
+    }
+
+    close(callback) {
+        this._server.close.apply(this._server, arguments)
+    }
+
+    address() {
+        return this._server.address()
+    }
 }
-
-Nicercast.prototype._handleRequest = function (req, res) {
-  switch (req.url) {
-    case '/':
-    case '/listen.m3u':
-      if (req.method === 'GET') {
-        this._playlist(req, res)
-      } else {
-        res.writeHead(405)
-        res.end('Method not allowed')
-      }
-
-      break
-    case '/listen':
-      if (req.method === 'GET') {
-        this._listen(req, res)
-      } else {
-        res.writeHead(405)
-        res.end('Method not allowed')
-      }
-
-      break
-    default:
-      res.writeHead(404)
-      res.end('Not found')
-  }
-}
-
-Nicercast.prototype._playlist = function (req, res) {
-  const urlProps = {
-    protocol: 'http',
-    pathname: '/listen'
-  }
-
-  if (req.headers.host) {
-    urlProps.host = req.headers.host
-  } else {
-    const info = req.socket.address()
-    urlProps.hostname = info.address
-    urlProps.port = info.port
-  }
-
-  res.writeHead(200, { 'Content-Type': 'audio/x-mpegurl' })
-  res.end(url.format(urlProps))
-}
-
-Nicercast.prototype._listen = function (req, res) {
-  const acceptsMetadata = (req.headers['icy-metadata'] === '1')
-
-  res.writeHead(200, {
-    'Connection': 'close',
-    'Content-Type': 'audio/mpeg',
-    'Icy-Metaint': (acceptsMetadata ? META_INTERVAL : undefined)
-  })
-
-  let output
-  if (acceptsMetadata) {
-    output = new icy.Writer(META_INTERVAL)
-    this._metadataStreams.push(output)
-    output.queueMetadata(this._metadata)
-    output.pipe(res)
-  } else {
-    output = res
-  }
-
-  const encoder = new lame.Encoder({
-    channels: CHANNELS,
-    bitDepth: SAMPLE_SIZE,
-    sampleRate: SAMPLE_RATE
-  })
-
-  this._listenStreams.push(encoder)
-
-  function teardown () {
-    removeValueFromArray(this._listenStreams, encoder)
-    removeValueFromArray(this._metadataStreams, output)
-
-    this._inputStream.unpipe(encoder)
-    encoder.unpipe(output)
-    encoder.end()
-  }
-
-  this._inputStream.pipe(encoder).pipe(output)
-  req.socket.on('close', teardown.bind(this))
-}
-
-Nicercast.prototype.setMetadata = function (metadata) {
-  this._metadata = metadata
-
-  this._metadataStreams.forEach(function (stream) {
-    stream.queue(metadata)
-  })
-}
-
-Nicercast.prototype.setInputStream = function (inputStream) {
-  const currentInput = this._inputStream
-
-  this._listenStreams.forEach(function (stream) {
-    currentInput.unpipe(stream)
-    inputStream.pipe(stream)
-  })
-
-  this._inputStream = inputStream
-}
-
-Nicercast.prototype.listen = function (port, hostname, backlog, callback) {
-  this._server.listen(port, hostname, backlog, () => {
-    // If a port of 0 is provided, the server will pick an appropriate port.
-    // ensure we invoke our callback with whatever port is running
-    callback(this._server.address().port)
-  })
-}
-
-Nicercast.prototype.close = function (callback) {
-  this._server.close.apply(this._server, arguments)
-}
-
-Nicercast.prototype.address = function () {
-  return this._server.address()
-}
-
-function start (port, callback) {
-  this.listen(port, undefined, undefined, callback)
-}
-
-function stop () {
-  try { this.close() } catch (err) {}
-}
-
-Nicercast.prototype.start = util.deprecate(start, '.start(port[, cb]) is deprecated, use .listen(port[, hostname][, backlog][, cb])')
-Nicercast.prototype.stop = util.deprecate(stop, '.stop() is deprecated, use .close([cb])')
 
 module.exports = Nicercast
